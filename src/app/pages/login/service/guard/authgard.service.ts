2a01084f18d7fa4a6960578569ee31a30c18b0ca
NTANTAME PETIEU Stephen <stephenpetieu@gmail.com>
2021-09-06 18:19:31 +0100

    guard updated

--- src/app/pages/login/service/guard/authgard.service.ts
+++ src/app/pages/login/service/guard/authgard.service.ts
@@ -17,11 +17,6 @@ export class AuthgardService implements CanActivate{
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if(this.loginService.isLoggedIn()) {
-      interval(1000).subscribe(
-        value => {
-          if(this.loginService.isLoggedOut()) this.router.navigate(['/pages/login']);
-        }
-      );
       console.log(this.loginService.isLoggedIn())
       return true;
     }
